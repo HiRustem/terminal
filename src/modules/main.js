8cb0983b5eeb88b51645066f400921a49c32be06
Rustem <rustem_hajrullin@enot.work>
2026-01-18 20:36:55 +0300

    feat: added token validation and semantic grammar parser

--- src/modules/main.js
+++ src/modules/main.js
@@ -1,15 +1,16 @@
-import validateTokens from "../constants/token/token.js";
+import parseInvocations from "../modules/semantic-grammar.js";
 import lexicalAnalysis from "./lexical-grammar.js";
 
 var inputElement = document.getElementById("input");
 
 inputElement.addEventListener("change", (event) => {
   var start = Date.now();
-  console.log(
-    validateTokens(
-      lexicalAnalysis(event.target.value)
-    )
-  );
+
+  var lexical = lexicalAnalysis(event.target.value);
+  var invocations = parseInvocations(lexical);
+
+  console.log('lexical', lexical);
+  console.log('invocations', invocations);
   var end = Date.now();
   console.log("milliseconds:", end - start);
 });
--- src/modules/semantic-grammar.js
+++ src/modules/semantic-grammar.js
@@ -1,22 +1,77 @@
-import { validate } from "./validators"
+import { METHOD_TOKEN_VARIANT_NAME, PACKAGE_TOKEN_VARIANT_NAME } from "../constants/token/keyword-token.js";
+import { TOKEN_PARSERS } from "../constants/token/token.js";
+import { returnInvalid, validate } from "./validators.js"
 
-var parseInvocations = (nodes) => (
-  nodes.reduce(
-    () => {
+var parseInvocations = (tokens) => (
+  tokens.reduce(
+    (accumulator, current, currentIndex, array) => {
+      if (!accumulator.isValid) {
+        return accumulator;
+      }
 
-    }, {
-      isValid: true,
-      valid: {
-        package: "",
-        method: "",
-        arguments: [],
-      } 
+      var parseHandler = TOKEN_PARSERS[current.valid.type];
+
+      if (!parseHandler) {
+        return returnInvalid(`No such parse handler for token with type: ${current.valid.type}`);
+      }
+
+      var parsedNode = parseHandler(
+        {
+          previous: array[currentIndex - 1]?.valid ?? null,
+          current: current?.valid,
+          next: array[currentIndex + 1]?.valid ?? null,
+        }
+      );
+
+      if (!parsedNode.isValid) {
+        accumulator = parsedNode;
+
+        return accumulator;
+      }
+
+      // console.log('parsedNode', parsedNode);
+
+      var updateAccumulator = PARSED_NODE_ACCUMULATOR_UPDATERS[parsedNode.valid.type];
+
+      if (!updateAccumulator) {
+        return returnInvalid(`No such accumulator update handler for node with type: ${parsedNode.valid.type}`)
+      }
+
+      accumulator.valid = updateAccumulator(parsedNode.valid, accumulator.valid);
+
+      return accumulator;
+  }, {
+    isValid: true,
+    valid: {
+      package: "",
+      method: "",
+      arguments: [],
+      flags: []
     }
-  )
+  })
 );
 
+var PARSED_NODE_ACCUMULATOR_UPDATERS = {
+  [PACKAGE_TOKEN_VARIANT_NAME]: (current, accumulator) => (
+    {
+      package: current.keyword,
+      method: accumulator.method,
+      arguments: accumulator.arguments,
+      flags: accumulator.flags
+    }
+  ),
+  [METHOD_TOKEN_VARIANT_NAME]: (current, accumulator) => (
+    {
+      package: accumulator.package,
+      method: current.keyword,
+      arguments: accumulator.arguments,
+      flags: accumulator.flags
+    }
+  )
+}
+
 export default (input) => validate(
-  ({ valid }) => (parseInvocations(valid)),
+  ({ valid }) => parseInvocations(valid),
 )(
   input
 );
